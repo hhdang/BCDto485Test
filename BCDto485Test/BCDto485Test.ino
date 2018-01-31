fc9db8ce29c45565d9e2436c1c869950263d99b6
hhdang <l>
2018-01-31 10:57:00 +0800

    添加项目文件。

---
+++ BCDto485Test/BCDto485Test.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		BCDto485Test.ino
+ Created:	2018-01-31 10:56:51
+ Author:	dang1
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
